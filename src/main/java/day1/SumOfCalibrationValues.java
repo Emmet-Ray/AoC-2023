fd36d0280d13ab0904d177620c7e852eed33a133
mengtaoli <2741847511@qq.com>
2023-12-31 10:58:11 +0800

    day 1 part 2

--- src/main/java/day1/SumOfCalibrationValues.java
+++ src/main/java/day1/SumOfCalibrationValues.java
@@ -7,27 +7,79 @@ import java.io.IOException;
 
 public class SumOfCalibrationValues {
 
+    public static class Pair {
+        public Pair(int increaseStep, int digit) {
+            this.increaseStep = increaseStep;
+            this.digit = digit;
+        }
+        int increaseStep;
+        int digit;
+
+        public void printPair() {
+            System.out.println("increaseStep: " + increaseStep + " digit: " + digit);
+        }
+    }
+
+    public static Pair LetterDigit(String input, int startIndex) {
+        int leftLength = input.length() - startIndex;
+        if (leftLength < 3) {
+            return new Pair(0, -1);
+        }
+        String first = input.substring(startIndex, startIndex + 3);
+        switch (first) {
+            case "one": return new Pair(2, 1);
+            case "two": return new Pair(2, 2);
+            case "six": return new Pair(2, 6);
+        }
+
+        if (leftLength > 3) {
+            String second = input.substring(startIndex, startIndex + 4);
+            switch (second) {
+                case "four": return new Pair(3, 4);
+                case "five": return new Pair(3, 5);
+                case "nine": return new Pair(3, 9);
+            }
+        }
+        if (leftLength > 4) {
+            String third = input.substring(startIndex, startIndex + 5);
+            switch (third) {
+                case "three": return new Pair(4, 3);
+                case "seven": return new Pair(4, 7);
+                case "eight": return new Pair(4, 8);
+            }
+        }
+        return new Pair(0, -1);
+    }
+
     /**
      *
      * @param input
      * @return the calibration value of the input string
      */
     public static int calibrationValueOfOneString(String input) {
+        System.out.println(input);
         int firstDigit, lastDigit;
         firstDigit = lastDigit = -1;
         char currentChar = 0;
         for (int i = 0; i < input.length(); i++) {
             currentChar = input.charAt(i);
-            if (firstDigit == -1 && Character.isDigit(currentChar)) {
-                firstDigit = 10 * (currentChar - '0');
+            if (Character.isDigit(currentChar)) {
                 lastDigit = currentChar - '0';
-            } else if (Character.isDigit(currentChar)) {
-                lastDigit = currentChar - '0';
+            } else {
+                Pair curPair = LetterDigit(input, i);
+                if (curPair.increaseStep != 0) {
+                    //i += curPair.increaseStep;      <----------------- this line is the misunderstanding line
+                    lastDigit = curPair.digit;
+                }
+            }
+
+            if (firstDigit == -1 && lastDigit != -1) {
+                firstDigit = 10 * lastDigit;
             }
         }
+        System.out.println(firstDigit + lastDigit);
         return firstDigit + lastDigit;
     }
-
 
     public static int sumOfCalibrationValues(String fileName) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(fileName));
--- src/test/java/day1/testCalibrationValueOfOneString.java
+++ src/test/java/day1/testCalibrationValueOfOneString.java
@@ -1,5 +1,6 @@
 package day1;
 
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,5 +14,68 @@ public class testCalibrationValueOfOneString {
        assertEquals(38, SumOfCalibrationValues.calibrationValueOfOneString("pqr3stu8vwx"));
        assertEquals(15, SumOfCalibrationValues.calibrationValueOfOneString("a1b2c3d4e5f"));
        assertEquals(77, SumOfCalibrationValues.calibrationValueOfOneString("treb7uchet"));
+   }
+
+    /**
+     * three partition :
+     * length 3, 4, 5
+     */
+   @Test
+   public void testLetterDigit() {
+       SumOfCalibrationValues.Pair result = SumOfCalibrationValues.LetterDigit("one", 0);
+       assertEquals(2, result.increaseStep);
+       assertEquals(1, result.digit);
+       result = SumOfCalibrationValues.LetterDigit("twodaf", 0);
+       assertEquals(2, result.increaseStep);
+       assertEquals(2, result.digit);
+       result = SumOfCalibrationValues.LetterDigit("six", 0);
+       assertEquals(2, result.increaseStep);
+       assertEquals(6, result.digit);
+
+       result = SumOfCalibrationValues.LetterDigit("four", 0);
+       assertEquals(3, result.increaseStep);
+       assertEquals(4, result.digit);
+       result = SumOfCalibrationValues.LetterDigit("five", 0);
+       assertEquals(3, result.increaseStep);
+       assertEquals(5, result.digit);
+       result = SumOfCalibrationValues.LetterDigit("nine", 0);
+       assertEquals(3, result.increaseStep);
+       assertEquals(9, result.digit);
+
+       result = SumOfCalibrationValues.LetterDigit("three", 0);
+       assertEquals(4, result.increaseStep);
+       assertEquals(3, result.digit);
+       result = SumOfCalibrationValues.LetterDigit("seven", 0);
+       assertEquals(4, result.increaseStep);
+       assertEquals(7, result.digit);
+       result = SumOfCalibrationValues.LetterDigit("eight", 0);
+       assertEquals(4, result.increaseStep);
+       assertEquals(8, result.digit);
+
+       result = SumOfCalibrationValues.LetterDigit("eight", 1);
+       assertEquals(0, result.increaseStep);
+   }
+
+   @Test
+    public void testDay1Puzzle2() {
+       assertEquals(29, SumOfCalibrationValues.calibrationValueOfOneString("two1nine"));
+       assertEquals(83, SumOfCalibrationValues.calibrationValueOfOneString("eightwothree"));
+       assertEquals(13, SumOfCalibrationValues.calibrationValueOfOneString("abcone2threexyz"));
+       assertEquals(24, SumOfCalibrationValues.calibrationValueOfOneString("xtwone3four"));
+       assertEquals(42, SumOfCalibrationValues.calibrationValueOfOneString("4nineeightseven2"));
+       assertEquals(14, SumOfCalibrationValues.calibrationValueOfOneString("zoneight234"));
+       assertEquals(76, SumOfCalibrationValues.calibrationValueOfOneString("7pqrstsixteen"));
+
+       assertEquals(38, SumOfCalibrationValues.calibrationValueOfOneString("three7938"));
+       assertEquals(92, SumOfCalibrationValues.calibrationValueOfOneString("nine92jnhgqzctpgbcbpz"));
+
+       assertEquals(22, SumOfCalibrationValues.calibrationValueOfOneString("two"));
+       assertEquals(22, SumOfCalibrationValues.calibrationValueOfOneString("2"));
+       assertEquals(23, SumOfCalibrationValues.calibrationValueOfOneString("twothree"));
+
+
+
+       // this test case is the tricky one
+       assertEquals(28, SumOfCalibrationValues.calibrationValueOfOneString("twothreeight"));
    }
 }
