11db39754c747e73f92d092fac9f1c6e42e9f69e
mengtaoli <2741847511@qq.com>
2024-01-21 19:39:09 +0800

    day9 part1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,16 @@ import day5.Seed;
 import day6.BoatGame;
 import day7.CamelCards;
 import day8.CamelMap;
+import day9.Day9;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-        String file = "C:\\Users\\Lenovo\\Desktop\\study\\AdventOfCode2023\\src\\main\\resources\\day8\\input.txt";
+        String file = "C:\\Users\\Lenovo\\Desktop\\study\\AdventOfCode2023\\src\\main\\resources\\day9\\input.txt";
         try {
-            long result = CamelMap.totalSteps(file);
+            int result = Day9.sum(file);
             System.out.println(result);
         } catch (IOException e) {
 
---
+++ src/main/java/day9/Day9.java
@@ -0,0 +1,63 @@
+package day9;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day9 {
+
+    private static boolean allZero(List<Integer> sequence) {
+        for (int i: sequence) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int nextValue(List<Integer> sequence) {
+        // base case
+        if (allZero(sequence)) {
+            return 0;
+        }
+
+        List<Integer> nextSequence = new ArrayList<>();
+        for (int i = 0; i < sequence.size() - 1; i++) {
+            int current = sequence.get(i + 1) - sequence.get(i);
+            nextSequence.add(current);
+        }
+        int next = nextValue(nextSequence);
+        return next + sequence.get(sequence.size() - 1);
+    }
+
+    private static List<Integer> stringListToIntList(String[] list) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < list.length; i++) {
+            int current = Integer.parseInt(list[i]);
+            result.add(current);
+        }
+        return result;
+    }
+    public static int sum(String file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String currentLine = reader.readLine();
+
+        int sum = 0;
+        while (currentLine != null) {
+            List<Integer> current = stringListToIntList(currentLine.split(" +"));
+            sum += nextValue(current);
+            currentLine = reader.readLine();
+        }
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        List<Integer> test = List.of(10, 13, 16, 21, 30, 45);
+        int result = nextValue(test);
+        System.out.println(result);
+    }
+}
+
